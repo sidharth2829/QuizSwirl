ee6d16bc41c06e31da7d23b4847f7a59277506b1
pankhuri92 <asthanap192@gmail.com>
2023-11-29 20:14:02 +0530

    added asset

--- login.php
+++ login.php
@@ -39,7 +39,7 @@ if (isset($_POST["login"]))
     <title>QuizSwirl-Login</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="/css/login.css">
 </head>
 
 <body>
--- signup.php
+++ signup.php
@@ -6,7 +6,7 @@
     <title>QuizSwirl-SignUp</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/signup.css">
+    <link rel="stylesheet" href="/css/signup.css">
     <script>
             function validateRegistrationForm() 
             {
